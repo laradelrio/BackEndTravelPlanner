139c2dd341238fa26362de43c3cd60fbbd8eb563
laradelrio <lara.delrio333@gmail.com>
2024-01-25 18:38:58 +0100

    cors origin change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:4200",
+    origin: "https://travel-mates.vercel.app",
     credentials: true,
 };
 
